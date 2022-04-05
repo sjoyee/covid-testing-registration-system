4b8d7d40bcf9fb51acd481b56ee461b974cf02ea
Siang Jo Yee <joyeesiang@gmail.com>
2022-04-05 17:54:17 +0800

    Setup frontend and backend for the project

---
+++ backend/src/main/java/com/fit3077/covidtestingregistration/CovidTestingRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.fit3077.covidtestingregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidTestingRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidTestingRegistrationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fit3077/covidtestingregistration/CovidTestingRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit3077.covidtestingregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTestingRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
