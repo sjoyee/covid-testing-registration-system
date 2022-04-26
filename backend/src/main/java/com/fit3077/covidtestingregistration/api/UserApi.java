fffcfdf3d2c5dd5e3ca79fe113f613ae84ed0572
Siang Jo Yee <jsia0008@student.monash.edu>
2022-04-26 17:40:35 +0800

    Added code for task 3 (On-site Booking)

--- backend/src/main/java/com/fit3077/covidtestingregistration/api/UserApi.java
+++ backend/src/main/java/com/fit3077/covidtestingregistration/api/UserApi.java
@@ -1,15 +1,16 @@
-package com.fit3077.covidtestingregistration.user;
+package com.fit3077.covidtestingregistration.api;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.security.InvalidKeyException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-public class UserRepository {
+public class UserApi {
 
     private static final String API_KEY = System.getenv("FIT3077_API_KEY");
 
@@ -17,9 +18,14 @@ public class UserRepository {
 
     private String rootUrl = "https://fit3077.com/api/v1/user";
 
-    public String authenticateUser(String jsonString) {
+    public String authenticateUser(String userName, String password) {
 
         String userLoginUrl = rootUrl + "/login";
+
+        String jsonString = "{" +
+                "\"userName\":\"" + userName + "\"," +
+                "\"password\":\"" + password + "\"" +
+                "}";
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(URI.create(userLoginUrl + "?jwt=true"))
@@ -67,7 +73,7 @@ public class UserRepository {
         return false;
     }
 
-    public ObjectNode[] getUsers() throws Exception {
+    public ObjectNode[] getUsers() {
 
         HttpClient client = HttpClient.newHttpClient();
 
@@ -81,10 +87,51 @@ public class UserRepository {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
             if (response.statusCode() != 200) {
-                throw new Exception("Please specify your API key");
+                throw new InvalidKeyException("Please specify your API key");
             }
 
             return new ObjectMapper().readValue(response.body(), ObjectNode[].class);
+
+        } catch (InterruptedException e0) {
+            e0.printStackTrace();
+            Thread.currentThread().interrupt();
+
+        } catch (IOException e1) {
+            e1.printStackTrace();
+
+        } catch (InvalidKeyException e2) {
+            e2.printStackTrace();
+        }
+
+        return new ObjectNode[0];
+
+    }
+
+    // return user id
+    public String createNewUser(ObjectNode userObject) {
+        String jsonString = "{" +
+                "\"givenName\":\"" + userObject.get("givenName").textValue() + "\"," +
+                "\"familyName\":\"" + userObject.get("familyName").textValue() + "\"," +
+                "\"userName\":\"" + userObject.get("userName").textValue() + "\"," +
+                "\"password\":\"" + userObject.get("password").textValue() + "\"," +
+                "\"phoneNumber\":\"" + userObject.get("phoneNumber").textValue() + "\"," +
+                "\"isCustomer\":" + userObject.get("isCustomer").asBoolean() + "," +
+                "\"isAdmin\":" + userObject.get("isAdmin").asBoolean() + "," +
+                "\"isHealthcareWorker\":" + userObject.get("isHealthcareWorker").asBoolean() +
+                "}";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(URI.create(rootUrl))
+                .setHeader(KEY_NAME, API_KEY)
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonString))
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            ObjectNode jsonNode = new ObjectMapper().readValue(response.body(), ObjectNode.class);
+
+            return jsonNode.get("id").textValue();
 
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -95,7 +142,7 @@ public class UserRepository {
 
         }
 
-        return new ObjectNode[0];
+        return null;
 
     }
 
