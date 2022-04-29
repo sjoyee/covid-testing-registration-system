c1def30c776e49a0fc41869df7bd411d1c154cd5
Siang Jo Yee <joyeesiang@gmail.com>
2022-04-29 19:02:37 +0800

    Fixed error in TestingSiteCollection class

--- backend/src/main/java/com/fit3077/covidtestingregistration/testingsite/TestingSiteCollection.java
+++ backend/src/main/java/com/fit3077/covidtestingregistration/testingsite/TestingSiteCollection.java
@@ -1,4 +1,5 @@
 package com.fit3077.covidtestingregistration.testingsite;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -8,19 +9,19 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fit3077.covidtestingregistration.api.TestingSiteApi;
 
 public class TestingSiteCollection {
+    private List<TestingSite> allSites = new ArrayList<>();
     private List<TestingSite> displaySiteList = new ArrayList<>();
 
-    public TestingSiteCollection(){
-        this.displaySiteList = createTestingSite();
+    public TestingSiteCollection() {
+        createTestingSite();
     }
-    private List<TestingSite> createTestingSite() {
+
+    private void createTestingSite() {
         TestingSiteApi testingSiteApi = new TestingSiteApi();
-        List<TestingSite> displaySiteList = new ArrayList<>();
 
         for (ObjectNode testingNode : testingSiteApi.getTestingSites()) {
 
             String id = testingNode.get("id").textValue();
-            System.out.println("here" +id);
             String name = testingNode.get("name").textValue();
             String description = testingNode.get("description").textValue();
             String websiteUrl = testingNode.get("websiteUrl").textValue();
@@ -29,39 +30,29 @@ public class TestingSiteCollection {
             String siteType = testingNode.get("additionalInfo").get("siteType").textValue();
             TestingSite createdSite = new TestingSite(id, name, description, websiteUrl, phoneNumber, address,
                     siteType);
-            displaySiteList.add(createdSite);
+            this.allSites.add(createdSite);
         }
-        return displaySiteList;
     }
 
     private List<TestingSite> filteredType(String inputType, String inputSuburb) {
-        
-        List<TestingSite> allSites = createTestingSite();
-        
-        Set<TestingSite> filteredSitesSet = new HashSet<TestingSite>();
+        Set<TestingSite> filteredSitesSet = new HashSet<>();
 
-        if (inputType == null && inputSuburb == null){
-            return allSites;
+        if (inputType.isEmpty() && inputSuburb.isEmpty()) {
+            return this.allSites;
         }
-        for (TestingSite testingSite: allSites){
-            if (inputType != null){
-                if (testingSite.getTestingSiteType().name() == inputType){
-                    filteredSitesSet.add(testingSite);
-                }
+        for (TestingSite testingSite : this.allSites) {
+            if (!inputType.isEmpty() && testingSite.getTestingSiteType().name().equals(inputType)) {
+                filteredSitesSet.add(testingSite);
             }
-            if(inputSuburb != null){
-                if (testingSite.getTestingSiteType().name()== inputType){
-                    filteredSitesSet.add(testingSite);
-                }
+            if (!inputSuburb.isEmpty() && testingSite.getAddress().getSuburb().equals(inputSuburb)) {
+                filteredSitesSet.add(testingSite);
             }
-            
+
         }
-        List<TestingSite> filteredSites = new ArrayList<>(filteredSitesSet);
-        return filteredSites;
+        return new ArrayList<>(filteredSitesSet);
     }
 
     private Address jsonToAddress(ObjectNode node) {
-       
         String street = node.get("address").get("street").textValue();
         double longitude = node.get("address").get("longitude").asDouble();
         double latitude = node.get("address").get("latitude").asDouble();
@@ -70,18 +61,16 @@ public class TestingSiteCollection {
         String suburb = node.get("address").get("suburb").textValue();
         String state = node.get("address").get("state").textValue();
         String street2 = node.get("address").get("street2").textValue();
-        System.out.println(suburb);
-        System.out.println(longitude);
+
         return new Address(latitude, longitude, unitNumber, street, street2, suburb, state, postcode);
     }
 
-    public void updateTestingSiteList(String inputType, String inputSuburb){
-        this.displaySiteList = filteredType(inputType, inputSuburb);
+    public void updateTestingSiteList(String inputType, String inputSuburb) {
+        this.displaySiteList = this.filteredType(inputType, inputSuburb);
     }
 
-    public List<TestingSite> getTestingSiteList(){
+    public List<TestingSite> getTestingSiteList() {
         return this.displaySiteList;
     }
-
 
 }
