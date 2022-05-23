9df4a658c3ffc5d31f9e0f619546ca54ba7b5245
Siang Jo Yee <jsia0008@student.monash.edu>
2022-05-24 02:31:33 +0800

    Update controller and facade classes for the new updates

--- backend/src/main/java/com/fit3077/covidtestingregistration/model/booking/BookingFacade.java
+++ backend/src/main/java/com/fit3077/covidtestingregistration/model/booking/BookingFacade.java
@@ -3,8 +3,6 @@ package com.fit3077.covidtestingregistration.model.booking;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fit3077.covidtestingregistration.model.booking.active.ActiveBooking;
-import com.fit3077.covidtestingregistration.model.booking.active.ActiveBookingCollection;
 import com.fit3077.covidtestingregistration.model.user.UserGenerator;
 
 public class BookingFacade {
