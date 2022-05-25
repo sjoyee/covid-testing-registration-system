3c8984d7f13a0238d0569993291e8901001fd04a
Chantelle Loh <chantelleywloh@gmail.com>
2022-05-26 00:48:04 +0800

    remove errors

--- backend/src/main/java/com/fit3077/covidtestingregistration/controller/BookingController.java
+++ backend/src/main/java/com/fit3077/covidtestingregistration/controller/BookingController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fit3077.covidtestingregistration.model.MainFacade;
 import com.fit3077.covidtestingregistration.model.booking.ActiveBooking;
-import com.fit3077.covidtestingregistration.model.booking.log.LogCollection;
+
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
