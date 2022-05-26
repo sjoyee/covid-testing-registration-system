a969c3ea81ec0ee0f452101eb08aad23f76328e1
Chantelle Loh <chantelleywloh@gmail.com>
2022-05-26 11:04:40 +0800

    add various notification

--- backend/src/main/java/com/fit3077/covidtestingregistration/model/booking/observer/BookingNotificationListener.java
+++ backend/src/main/java/com/fit3077/covidtestingregistration/model/booking/observer/BookingNotificationListener.java
@@ -5,8 +5,22 @@ import com.fit3077.covidtestingregistration.model.booking.ActiveBooking;
 public class BookingNotificationListener implements BookingEventListener{
     private String notification;
 
-    public BookingNotificationListener(String notification) {
-        this.notification = notification;
+    public BookingNotificationListener() {
+        
+    }
+
+    public void setModificationNotification(String newTestSite, String newDateTime, String receptionistId,String bookingId){
+        this.notification = "Booking No. " + bookingId + " has been modified by Admin No. "+ receptionistId 
+        + "\n Updated: Testing Site No. " + newTestSite + ", Start Time : " + newDateTime; 
+    }
+    public void setCancelledNotification( String receptionistId,String bookingId){
+        this.notification = "Booking No. " + bookingId + " has been cancelled by Admin No. "+ receptionistId ; 
+    }
+    public void setAddNotification(String receptionistId,String bookingId){
+        this.notification = "Booking No. " + bookingId + " has been created by Admin No. "+ receptionistId ; 
+    }
+    public void setDeleteNotification(String receptionistId,String bookingId){
+        this.notification = "Booking No. " + bookingId + " has been deleted by Admin No. "+ receptionistId ; 
     }
 
 
